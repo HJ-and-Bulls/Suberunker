4f245a2d5d7a9bb1b7cb462cb1464359ea109d33
leesy015599 <leesy015599@gmail.com>
2023-09-11 20:18:30 +0900

    Add Constants class

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    public const int ResolutionWidth = 760;
+    public const int ResolutionHeight = 1280;
+    public const float UnitWidth = ResolutionWidth / 128f; // 거의 6
+    public const float UnitHeight = ResolutionHeight / 128f; // 딱 10
+
+    public const float PoopWidth = 1;
+    public const float PoopHeight = 1;
+}
